00dff93ceae69e6e802fb8194543cee6dbeb1934
Dan Guo <danguo1113@gmail.com>
2013-08-25 11:42:58 -0700

    basic functionality working

--- mapize_campsites/campsites_finder/views.py
+++ mapize_campsites/campsites_finder/views.py
@@ -6,6 +6,22 @@ import crawl_for_lst
 class SubmitURLForm(forms.Form):
     url_to_crawl = forms.CharField()
 
+def create_location_lst(lst_of_locations):
+    location_lst_formatted = []
+    for tup in enumerate(lst_of_locations):
+        try:
+            key = tup[1]
+            curr_location_lst = []
+            loc_data = crawl_for_lst.get_map_for_location(key)
+            curr_location_lst.append(key.encode('utf-8'))
+            curr_location_lst.append(loc_data['lat'])
+            curr_location_lst.append(loc_data['lng'])
+            curr_location_lst.append(tup[0])
+            location_lst_formatted.append(curr_location_lst)
+        except KeyError:
+            print 'No data'
+    return location_lst_formatted
+
 def submit_form(request):
     if request.method == 'POST':
         form = SubmitURLForm(request.POST)
@@ -13,7 +29,9 @@ def submit_form(request):
             # Do stuff
             url_to_crawl = form.cleaned_data['url_to_crawl']
             lst_of_locations = crawl_for_lst.get_lst_of_locations(url_to_crawl)
-            context = {'lst_of_locations': lst_of_locations}
+            location_lst_formatted = create_location_lst(lst_of_locations)
+            print location_lst_formatted
+            context = {'location_lst_formatted':location_lst_formatted}
             return render_to_response('crawl_result.html',context_instance=RequestContext(request,context))
     else:
         form = SubmitURLForm()
--- mapize_campsites/crawl_for_lst.py
+++ mapize_campsites/crawl_for_lst.py
@@ -13,11 +13,28 @@ def format_lst_of_locations(lst_of_locations):
         formatted_lst.append(elem_str_split[1])
     return formatted_lst
 
-def get_a_map_for_location(location_name):
+
+def get_map_for_location(location_name):
     API_KEY = 'AIzaSyCgv0Mm-4kUoyLP2pD8qnmZnyCuXH6hosc'
-    maps_request_api_url = 'https://maps.googleapis.com/maps/api/place/autocomplete/json?input=Vict&types=geocode&sensor=true&key='
-    response = urllib2.urlopen(maps_request_api_url + API_KEY)
+    if location_name.count(',') > 1:
+        location_name = re.findall(r'^[^,]*, [^,]*,', location_name, re.I)[0]
+    delimited_location = location_name.replace(" ", "+")
+    response = urllib2.urlopen('http://maps.googleapis.com/maps/api/geocode/json?address=' + delimited_location + '&sensor=true')
     json_dict = json.load(response)
+    results_dict = {'geo_found':True}
+    geo = {}
+    try:
+        geo = json_dict['results'][0]['geometry']
+    except IndexError:
+        results_dict = {'geo_found':False}
+        return results_dict
+    if 'location' in geo:
+        return geo['location']
+    elif 'bounds' in geo:
+        return geo['bounds']['northeast']
+    else:
+        return {}
+
 
 def get_lst_of_locations(url_to_crawl):
     response = urllib2.urlopen(url_to_crawl)
