51db94a2ebf1a52dcc20e77c8be59db930023040
Dan Guo <danguo1113@gmail.com>
2013-08-24 23:11:36 -0700

    Start ed to integrate maps

--- mapize_campsites/crawl_for_lst.py
+++ mapize_campsites/crawl_for_lst.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup as bs
 import urllib2
 import sys
 import re
-
+import json
 
 def format_lst_of_locations(lst_of_locations):
     formatted_lst = []
@@ -13,6 +13,11 @@ def format_lst_of_locations(lst_of_locations):
         formatted_lst.append(elem_str_split[1])
     return formatted_lst
 
+def get_a_map_for_location(location_name):
+    API_KEY = 'AIzaSyCgv0Mm-4kUoyLP2pD8qnmZnyCuXH6hosc'
+    maps_request_api_url = 'https://maps.googleapis.com/maps/api/place/autocomplete/json?input=Vict&types=geocode&sensor=true&key='
+    response = urllib2.urlopen(maps_request_api_url + API_KEY)
+    json_dict = json.load(response)
 
 def get_lst_of_locations(url_to_crawl):
     response = urllib2.urlopen(url_to_crawl)
